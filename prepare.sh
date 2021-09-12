c71c6c952bbb643a4049a6252030d0bd21023a43
pshih6 <oo-hey@hotmail.com>
2021-09-11 22:13:23 -0400

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 mkdir checkpoints
 
-mkdir checkpoints/wn
+mkdir checkpoints/codex-s
+mkdir checkpoints/codex-m
+mkdir checkpoints/codex-l
 mkdir checkpoints/fb
-mkdir checkpoints/kinship
-mkdir checkpoints/umls
-mkdir checkpoints/nell
 
-mkdir checkpoints/wn/out
 mkdir checkpoints/fb/out
-mkdir checkpoints/kinship/out
-mkdir checkpoints/umls/out
-mkdir checkpoints/nell/out
+mkdir checkpoints/codex-s/out
+mkdir checkpoints/codex-m/out
+mkdir checkpoints/codex-l/out
 
-mkdir checkpoints/wn/out/conv
 mkdir checkpoints/fb/out/conv
-mkdir checkpoints/kinship/out/conv
-mkdir checkpoints/umls/out/conv
-mkdir checkpoints/nell/out/conv
+mkdir checkpoints/codex-s/conv
+mkdir checkpoints/codex-m/out/conv
+mkdir checkpoints/codex-l/out/conv
